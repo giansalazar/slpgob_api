196f8c98b74d09bdf174fd09aad7e9d6a9f2abc4
Giancarlo Salazar <giancarlosalazar97@gmail.com>
2022-11-22 18:33:20 -0600

    updateRequirements

--- prueba/settings.py
+++ prueba/settings.py
@@ -44,19 +44,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'personal',
 
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -65,10 +65,7 @@ MIDDLEWARE = [
     
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'https://giansalazar.github.io/react-gob',
-    'http://localhost:5173',
-)
+CORS_ALLOW_ALL_ORIGINS=True
 
 ROOT_URLCONF = 'prueba.urls'
 
